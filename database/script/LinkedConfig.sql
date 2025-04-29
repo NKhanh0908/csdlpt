369979ebef7a330e7e11d5480a1a79970692db3d
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2025-04-29 19:11:03 +0700

    Linked server con

--- database/script/LinkedConfig.sql
+++ database/script/LinkedConfig.sql
@@ -4,32 +4,19 @@ select * from LINKEDSV3.chdidong.dbo.chinhanh;
 
 EXEC sp_linkedservers;
 
+-- SERVER 1
 EXEC sp_addlinkedserver 
     @server='LINKEDSV2', 
-    @srvproduct='SQL Server', 
+    @srvproduct='', 
     @provider='SQLNCLI', 
-    @datasrc='LAPTOP-O2NQEJ35\MSSQLSERVER2';
+    @datasrc='LAPTOP-\';
 GO
 
 EXEC sp_addlinkedserver 
     @server='LINKEDSV3', 
-    @srvproduct='SQL Server', 
-    @provider='SQLNCLI', 
-    @datasrc='LAPTOP-O2NQEJ35\MSSQLSERVER3';
-GO
-
-EXEC sp_addlinkedserver 
-    @server='LINKEDSV4', 
-    @srvproduct='SQL Server', 
-    @provider='SQLNCLI', 
-    @datasrc='LAPTOP-O2NQEJ35\MSSQLSERVER4';
-GO
-
-EXEC sp_addlinkedserver 
-    @server='MASTER', 
     @srvproduct='', 
     @provider='SQLNCLI', 
-    @datasrc='LAPTOP-O2NQEJ35';
+    @datasrc='LAPTOP-\';
 GO
 
 EXEC sp_addlinkedsrvlogin 
@@ -37,29 +24,76 @@ EXEC sp_addlinkedsrvlogin
     @useself='false', 
     @locallogin=NULL, 
     @rmtuser='sa',      
-    @rmtpassword='13524679';
+    @rmtpassword='';
 GO
 
 EXEC sp_addlinkedsrvlogin 
     @rmtsrvname='LINKEDSV3', 
     @useself='false', 
     @locallogin=NULL, 
-    @rmtuser='sa',       -- Tài khoản trên server từ xa
-    @rmtpassword='13524679';
+    @rmtuser='sa',       
+    @rmtpassword='';
+GO
+
+-- Server 2
+
+EXEC sp_addlinkedserver 
+    @server='LINKEDSV1', 
+    @srvproduct='', 
+    @provider='SQLNCLI', 
+    @datasrc='LAPTOP-\';
+GO
+
+EXEC sp_addlinkedserver 
+    @server='LINKEDSV3', 
+    @srvproduct='', 
+    @provider='SQLNCLI', 
+    @datasrc='LAPTOP-\';
 GO
 
 EXEC sp_addlinkedsrvlogin 
-    @rmtsrvname='LINKEDSV4', 
-    @useself='false', 
-    @locallogin=NULL, 
-    @rmtuser='sa',       -- Tài khoản trên server từ xa
-    @rmtpassword='13524679';
-GO
-
-EXEC sp_addlinkedsrvlogin 
-    @rmtsrvname='MASTER', 
+    @rmtsrvname='LINKEDSV1', 
     @useself='false', 
     @locallogin=NULL, 
     @rmtuser='sa',      
-    @rmtpassword='13524679';
+    @rmtpassword='';
+GO
+
+EXEC sp_addlinkedsrvlogin 
+    @rmtsrvname='LINKEDSV3', 
+    @useself='false', 
+    @locallogin=NULL, 
+    @rmtuser='sa',       
+    @rmtpassword='';
+GO
+
+-- Server 3
+EXEC sp_addlinkedserver 
+    @server='LINKEDSV1', 
+    @srvproduct='', 
+    @provider='SQLNCLI', 
+    @datasrc='LAPTOP-\';
+GO
+
+EXEC sp_addlinkedserver 
+    @server='LINKEDSV2', 
+    @srvproduct='', 
+    @provider='SQLNCLI', 
+    @datasrc='LAPTOP-\';
+GO
+
+EXEC sp_addlinkedsrvlogin 
+    @rmtsrvname='LINKEDSV1', 
+    @useself='false', 
+    @locallogin=NULL, 
+    @rmtuser='sa',      
+    @rmtpassword='';
+GO
+
+EXEC sp_addlinkedsrvlogin 
+    @rmtsrvname='LINKEDSV2', 
+    @useself='false', 
+    @locallogin=NULL, 
+    @rmtuser='sa',       
+    @rmtpassword='';
 GO
